a06a4c82a16e1124db61af694cf314b860831035
simonmumina <simonmumina2000@gmail.com>
2026-02-10 15:13:41 +0300

    Add project

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,37 +1,31 @@
-/// <reference path="./subjects/Teacher.ts" />
-/// <reference path="./subjects/Subject.ts" />
-/// <reference path="./subjects/Cpp.ts" />
-/// <reference path="./subjects/Java.ts" />
-/// <reference path="./subjects/React.ts" />
+// Interfaces with branding
+export interface MajorCredits {
+  credits: number;
+  readonly __brand: "MajorCredits";
+}
 
-import Subjects = Subjects;
+export interface MinorCredits {
+  credits: number;
+  readonly __brand: "MinorCredits";
+}
 
-// Export constants for subjects
-export const cpp = new Cpp();
-export const java = new Java();
-export const react = new React();
+// Functions
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits,
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: "MajorCredits",
+  };
+}
 
-// Export Teacher object
-export const cTeacher: Teacher = {
-  firstName: "Guillaume",
-  lastName: "Salva",
-  experienceTeachingC: 10,
-};
-
-// ----- Cpp -----
-console.log("C++");
-cpp.setTeacher(cTeacher);
-console.log(cpp.getRequirements());
-console.log(cpp.getAvailableTeacher());
-
-// ----- Java -----
-console.log("Java");
-java.setTeacher(cTeacher);
-console.log(java.getRequirements());
-console.log(java.getAvailableTeacher());
-
-// ----- React -----
-console.log("React");
-react.setTeacher(cTeacher);
-console.log(react.getRequirements());
-console.log(react.getAvailableTeacher());
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits,
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: "MinorCredits",
+  };
+}
---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,37 @@
+/// <reference path="./subjects/Teacher.ts" />
+/// <reference path="./subjects/Subject.ts" />
+/// <reference path="./subjects/Cpp.ts" />
+/// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
+
+import Subjects = Subjects;
+
+// Export constants for subjects
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
+
+// Export Teacher object
+export const cTeacher: Teacher = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+  experienceTeachingC: 10,
+};
+
+// ----- Cpp -----
+console.log("C++");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// ----- Java -----
+console.log("Java");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// ----- React -----
+console.log("React");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
